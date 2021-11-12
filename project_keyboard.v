f402d77b1ac18be0945f57a5c9c58342fe502ec0
brandenap1 <86215396+brandenap1@users.noreply.github.com>
2021-11-12 11:03:40 -0500

    Add files via upload
    project_keyboard.v : decodes data from PS2 keyboard to respective attack type
    
    Debouncing cases:
    - displays idle states for both players when in any mode but gameplay
    - once a key is pressed, a debounce counter is started and reg value b is set high 
        - next cycle, reg value b is set low
        - when the counter limit is reached (data packet is sent), the counter is restarted and reg value b is high again

--- project_keyboard.v
+++ project_keyboard.v
@@ -20,82 +20,82 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module project_keyboard(input wire i_clk_kb,
-input wire i_round_start,
-input wire i_data, //key stroke to be sent
+module project_keyboard(input wire clk_kb,
+input wire round_start,
+input wire data, //key stroke to be sent
 //output reg [3:0] led1 //LED set 1 for 1st value
-output reg [3:0] o_data
+output reg [3:0] out
     );
-reg [7:0] r_data_curr; //holder value for data case statement
-reg [3:0] o_data_temp; //data to be sent to led1 to be output
-reg [3:0] r_bit; 
-reg r_flag;
-reg r_debounce;
+reg [7:0] data_curr; //holder value for data case statement
+reg [3:0] data_out; //data to be sent to led1 to be output
+reg [3:0] b; 
+reg flag;
+reg debounce;
 
 initial 
 begin 
     b<=4'h1; 
-    r_flag<=1'b0; 
-    r_data_curr<=8'h45; 
-    o_data<=4'b0000;
-    o_data_temp<=4'b0000;
+    flag<=1'b0; 
+    data_curr<=8'h45; 
+    out<=4'b0000;
+    data_out<=4'b0000;
 end
 
 //value 1
-always @(negedge i_clk_kb) //Active for negative edge of clk from keyboard 
+always @(negedge clk_kb) //Active for negative edge of clk from keyboard 
 begin 
 
-    case(r_bit) 
+    case(b) 
     1:; //first bit 
-    2:r_data_curr[0]<=o_data; 
-    3:r_data_curr[1]<=o_data; 
-    4:r_data_curr[2]<=o_data; 
-    5:r_data_curr[3]<=o_data; 
-    6:r_data_curr[4]<=o_data; 
-    7:r_data_curr[5]<=o_data; 
-    8:r_data_curr[6]<=o_data; 
-    9:r_data_curr[7]<=o_data; 
-    10:r_flag<=1'b1; //Parity bit 
-    11:r_flag<=1'b0; //Ending bit 
+    2:data_curr[0]<=data; 
+    3:data_curr[1]<=data; 
+    4:data_curr[2]<=data; 
+    5:data_curr[3]<=data; 
+    6:data_curr[4]<=data; 
+    7:data_curr[5]<=data; 
+    8:data_curr[6]<=data; 
+    9:data_curr[7]<=data; 
+    10:flag<=1'b1; //Parity bit 
+    11:flag<=1'b0; //Ending bit 
     
     endcase 
-     if(r_bit == 1) 
-        r_debounce <= 1;
+     if(b == 1) 
+        debounce <= 1;
      else
-        r_debounce <= 0;
-     if(r_bit<=10) 
-        r_bit<=r_bit+1'b1; 
+        debounce <= 0;
+     if(b<=10) 
+        b<=b+1'b1; 
      
-     else if(r_bit==11) 
-        r_bit<=1; 
+     else if(b==11) 
+        b<=1; 
  
 end 
  
-always @(negedge i_clk_kb) //Active for negative edge of clk from keyboard  
+always @(negedge clk_kb) //Active for negative edge of clk from keyboard  
     begin
     //value 1 from hex on keyboard to binary to be displayed 
-    if (i_round_start) begin
-            case (r_data_curr)
-            8'h1C: o_data_temp = 4'b0001; //letter a for user 1 light punch 
-            8'h23: o_data_temp = 4'b0010; //letter d for user 1 heavy punch
-            8'h29: o_data_temp = 4'b0011; // space bar
+    if (round_start) begin
+            case (data_curr)
+            8'h1C: data_out = 4'b0001; //letter a for user 1 light punch 
+            8'h23: data_out = 4'b0010; //letter d for user 1 heavy punch
+            8'h29: data_out = 4'b0011; // space bar
             endcase
     end
     else
-        o_data_temp = 4'b0000;
+        data_out = 4'b0000;
 end  
 //    8'h05: data_out = 4'b0011; //F1 key for user 2 left punch
 //    8'h04: data_out = 4'b0100; //F3 key for user 2 right punch
         
-always@(posedge r_flag) // Printing data obtained to led set 1 (0,1,2,3)
+always@(posedge flag) // Printing data obtained to led set 1 (0,1,2,3)
 begin 
-     if(o_data_temp==4'b0001)
-         o_data[3:0]<= o_data_temp; 
-     else if(o_data_temp==4'b0010)
-         o_data[3:0]<= o_data_temp; 
-     else if(o_data_temp==4'b0011)
-         o_data[3:0]<= o_data_temp;
+     if(data_out==4'b0001)
+         out[3:0]<=data_out; 
+     else if(data_out==4'b0010)
+         out[3:0]<=data_out; 
+     else if(data_out==4'b0011)
+         out[3:0]<=data_out;
      else
-         o_data[3:0]<=o_data_temp;
+         out[3:0]<=data_out;
 end        
 endmodule
